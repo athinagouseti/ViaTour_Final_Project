060a997f7cb19fdebccab465820a83739395e8e7
carloszubillaga <car.zubillaga@gmail.com>
2021-11-29 18:05:29 +0000

    Added Location and Trip classes

---
+++ server/src/main/java/models/Trip.java
@@ -0,0 +1,58 @@
+package models;
+
+import java.util.List;
+
+public class Trip {
+
+    private Long id;
+    private String name;
+    private List<Location> locations;
+    private List<User> users;
+    private List<Image> images;
+
+    public Trip(String name) {
+        this.name = name;
+    }
+
+    public Trip() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Location> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(List<Location> locations) {
+        this.locations = locations;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<Image> getImages() {
+        return images;
+    }
+
+    public void setImages(List<Image> images) {
+        this.images = images;
+    }
+}
