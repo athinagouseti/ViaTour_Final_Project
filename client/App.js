cc30124ddcaaaf638558974536a17add3fae1cde
Athina Gouseti <athina.gouseti@outlook.com>
2021-11-29 22:28:41 +0000

    removed react native app.js  items and installed dependencies

--- client/App.js
+++ client/App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import Container from './components/container/Container';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <Container />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
