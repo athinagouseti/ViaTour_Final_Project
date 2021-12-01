001a4f6a57458ddc8e08f5f152a829e3f2f9480c
carloszubillaga <car.zubillaga@gmail.com>
2021-12-01 16:27:53 +0000

    Added Day and Activity classes

---
+++ server/src/main/java/com/viatour/server/models/Activity.java
@@ -0,0 +1,47 @@
+package com.viatour.server.models;
+
+import java.sql.Time;
+
+public class Activity {
+
+    private Long id;
+    private String description;
+    private Location location;
+    private Time timeSlot;
+
+    public Activity() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Time getTimeSlot() {
+        return timeSlot;
+    }
+
+    public void setTimeSlot(Time timeSlot) {
+        this.timeSlot = timeSlot;
+    }
+}
---
+++ server/src/main/java/com/viatour/server/models/Day.java
@@ -0,0 +1,10 @@
+package com.viatour.server.models;
+
+import java.sql.Date;
+import java.util.List;
+
+public class Day {
+
+    private Date date;
+    private List<Activity> activities;
+}
