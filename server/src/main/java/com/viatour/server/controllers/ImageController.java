de33e74d4b8dc5f0647b94eeef494f171a40c756
carloszubillaga <car.zubillaga@gmail.com>
2021-12-02 14:14:39 +0000

    Added ImageController file

---
+++ server/src/main/java/com/viatour/server/controllers/ImageController.java
@@ -0,0 +1,23 @@
+package com.viatour.server.controllers;
+
+import com.viatour.server.models.Image;
+import com.viatour.server.repositories.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ImageController {
+
+    @Autowired
+    ImageRepository imageRepository;
+
+    @GetMapping(value = "/images")
+    public ResponseEntity<List<Image>> getAllImages() {
+        return new ResponseEntity<>(imageRepository.findAll(), HttpStatus.OK);
+    }
+}
