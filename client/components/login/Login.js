3d6cfa316a312f5760dbae94de9d6c004178754d
Athina Gouseti <athina.gouseti@outlook.com>
2021-12-02 14:26:59 +0000

    tidied up login code

--- client/components/login/Login.js
+++ client/components/login/Login.js
@@ -1,37 +1,75 @@
-import React from 'react'
-import { StyleSheet, View, Button } from 'react-native'
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity } from "react-native";
 
-const Login = ({navigation}) => {
-    return(
-        <View style={styles.loginContainer}>
-            <Button
-                title="Log In"
-                onPress={() => {
-                    navigation.navigate("Landing Page")
-                }}
-            />
-            <Button
-                title="Create Account"
-                onPress={() => {
-                    navigation.navigate("Create Account")
-                }}
-            />
-            <Button
-                title="Forgot Password?"
-                onPress={() => {
-                    navigation.navigate("Reset Password")
-                }}
-            />
+const Login = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    return (
+        <View>
+        <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email."
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        />
+        </View>
+ 
+        <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password."
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+        </View>
+ 
+        <TouchableOpacity>
+        <Text style={styles.forgot_button}>Forgot Password?</Text>
+        </TouchableOpacity>
+ 
+        <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    loginContainer: {
-        width: 150,
-        height: 150,
-        padding: 60,
-      }
-})
+    inputView: {
+      backgroundColor: "#FAD02B",
+      borderRadius: 30,
+      width: "70%",
+      height: 45,
+      marginBottom: 20,
+   
+      alignItems: "center",
+    },
+   
+    TextInput: {
+      height: 50,
+      flex: 1,
+      padding: 10,
+      marginLeft: 20,
+    },
+   
+    forgot_button: {
+      height: 30,
+      marginBottom: 30,
+    },
+   
+    loginBtn: {
+      width: "60%",
+      borderRadius: 25,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 40,
+      backgroundColor: "#FAB52B",       
+    }
+    })
 
 export default Login;
--- client/components/profile/Profile.js
+++ client/components/profile/Profile.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { View, Text } from 'react-native';
-import Log from "../login/Log";
 import Login from "../login/Login";
-import LoginScreen from "../login/LoginScreen";
+
 
 const Profile = () => {
 
     return(
         <View>
-            <Log />
+            <Login />
         </View>
     )
 }
