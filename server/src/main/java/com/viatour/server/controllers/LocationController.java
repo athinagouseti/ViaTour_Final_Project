8162f97aa02e1d55394a6dc6a0954616f7124f34
carloszubillaga <car.zubillaga@gmail.com>
2021-12-02 14:09:07 +0000

    Added LocationController file

---
+++ server/src/main/java/com/viatour/server/controllers/LocationController.java
@@ -0,0 +1,23 @@
+package com.viatour.server.controllers;
+
+import com.viatour.server.models.Location;
+import com.viatour.server.repositories.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class LocationController {
+
+    @Autowired
+    LocationRepository locationRepository;
+
+    @GetMapping(value = "/locations")
+    public ResponseEntity<List<Location>> getAllLocations() {
+        return new ResponseEntity<>(locationRepository.findAll(), HttpStatus.OK);
+    }
+}
