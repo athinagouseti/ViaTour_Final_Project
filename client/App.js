ec5e1d1234f5731332522b08eeef1e940a20fb09
Athina Gouseti <athina.gouseti@outlook.com>
2021-12-08 19:04:44 +0000

    added code to remove warnings in simulator for demo

--- client/App.js
+++ client/App.js
@@ -2,9 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import Container from './components/container/Container';
-import { NavigationContainer } from '@react-navigation/native';
+import { LogBox } from 'react-native';  // impport to help remove warnings in ReactNative Simulator
 
 export default function App() {
+
+  // LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+  LogBox.ignoreAllLogs();//Ignore all log notifications
+
   return (
     <>
       <StatusBar style="auto"/>
